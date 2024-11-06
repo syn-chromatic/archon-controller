196be5fa76bf007dbc3865108e2f976b7b981db6
syn-chromatic <synchromatic.github@gmail.com>
2024-11-06 19:24:02 +0200

    refactor: change utils method

--- archon-transmitter/src/interface/utils.rs
+++ archon-transmitter/src/interface/utils.rs
@@ -15,7 +15,7 @@ pub fn discovery_to_menu_items(
     let mut items: Vec<_> = Vec::new();
 
     for (idx, info) in discovered.iter().enumerate() {
-        let title_text: String = " ".to_string() + info.announce_info().name();
+        let title_text: String = info.announce_info().name().to_string();
         let value: SubMenuSelect = SubMenuSelect::new(idx);
         let item: MenuItem<String, Option<usize>, SubMenuSelect, true> =
             MenuItem::new(title_text, value).with_value_converter(|select| select.index());
